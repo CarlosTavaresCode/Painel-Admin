259b7a885bf9b1a58a853a50a33fe37f81c30355
Carlos Tavares <83681104+CarlosTavaresCode@users.noreply.github.com>
2025-04-08 22:43:36 -0300

    Projeto finalizado | fazendo upload dos arquivos

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// adicionando hover na classe da lista de items
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
